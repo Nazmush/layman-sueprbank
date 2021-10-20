dea7fd53c4a6971e9be8032d947ccdf31d2c128e
Nazmush <nazmus.sadat@northsouth.edu>
2021-10-20 23:29:29 +0600

    Add project files.

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000 );
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammok");
+            Console.WriteLine(account.Balance);
+
+            account.MakeWithdrawal(50, DateTime.Now, "PS5 games");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
